8952be6885596b7bd32e6f6e3d274e0c77076288
lin <1195196992@qq.com>
2021-03-22 16:17:07 +0800

    lin 2021/3/23

---
+++ src/main/java/com/bjpowernode/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.dao;
+
+import com.bjpowernode.domain.Student;
+
+public class StudentDao {
+    int StudentAdd(Student student){
+        int res = 1;
+        return res;
+    }
+}
